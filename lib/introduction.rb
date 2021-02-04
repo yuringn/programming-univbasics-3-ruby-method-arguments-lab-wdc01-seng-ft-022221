610006bf14180ba9c3a5abd5a99870856e552df3
Yuri Ng <yuringuyen312@gmail.com>
2021-02-04 17:05:18 +0000

    Automatically backed up by Learn

--- lib/introduction.rb
+++ lib/introduction.rb
@@ -15,6 +15,7 @@ introduction_with_language(Yuri, Python)
 def introduction_with_language_optional(name, language ="Ruby")
   puts "Hi, my name is #{name} and I am learning to program in #{language}."
 end
+introduction_with_language_optional(Yuri)
 
 
 
